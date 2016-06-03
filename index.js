f0c60384dc3af366633a8958237b824a923aa6fb
Alan Savage <alansavage.dev@gmail.com>
2016-06-03 22:23:38 +0100

    Changed to export a single method for simpler.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 'use strict';
 
-class EnvVar {
-    static getValue(variableName, defaultValue) {
-        if (process.env[variableName]) {
-            return process.env[variableName];
-      }
-      console.warn(`Environment variable ${variableName} not specified, using default value: ${defaultValue}`);
-      return defaultValue;
+exports.getValue = function(variableName, defaultValue) {
+    if (process.env[variableName]) {
+        return process.env[variableName];
     }
+    console.warn(`Environment variable ${variableName} not specified, using default value: ${defaultValue}`);
+    return defaultValue;}
 }
-
-module.exports = EnvVar;
